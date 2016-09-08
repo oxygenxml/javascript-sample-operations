e6987deb44dbc6f5f50bcf3b4bfdc5c5194824b1
Radu Coravu <radu_coravu@sync.ro>
2016-09-08 12:10:34 +0300

    Update start-process.js

--- start-process/start-process.js
+++ start-process/start-process.js
@@ -1,4 +1,4 @@
 function doOperation(){
-    //Execute a command line which opens the Notepad application
-    Packages.java.lang.Runtime.getRuntime().exec("notepad.exe");
+    //Execute a command line which opens the Notepad application, the current folder is the project folder.
+    Packages.java.lang.Runtime.getRuntime().exec("notepad.exe", null, new Packages.java.io.File(new Packages.java.net.URL("${pdu}").toURI()));
 }
